dd6256c1e5826cd33a721a013c08e1997d5c55ca
DevaIrfan <devairfan65@gmail.com>
2025-12-07 21:42:11 +0700

    rer

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -8,12 +8,6 @@ const options = {
       version: '1.0.0',
       description: 'Dokumentasi API Ticket Service menggunakan Swagger'
     },
-    servers: [
-      {
-        url: 'http://localhost:4000',
-        description: 'Local server'
-      }
-    ]
   },
   apis: ['./src/api/*.js'], // Scan semua file endpoint
 };
